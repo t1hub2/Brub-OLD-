bf3fee79d5f14ba3e3623a45799b63620d39f468
t1hub2 <solavolodimir@gmail.com>
2025-01-17 12:30:52 +0100

    Update .lua

--- .lua
+++ .lua
@@ -1,4 +1,4 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet(("https://raw.githubusercontent.com/shlexware/Orion/main/source")))()
 local Window = OrionLib:MakeWindow({Name = "T1 hub / Первый русский скрипт!!", HidePremium = false, IntroText = "T1 hub" , SaveConfig = true, ConfigFolder = "OrionTest"})
 
 OrionLib:MakeNotification({
